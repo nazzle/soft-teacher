b87ef475d91ab33e6a92fb9e2fc5dd74eca121c3
Israel Kamendu <israelkamendu@iNazzle.local>
2023-06-19 13:38:05 +0300

    Linked downloads to files

--- src/components/constants/api.js
+++ src/components/constants/api.js
@@ -1,1 +1,4 @@
 export const API_BASE_URL = 'http://localhost:1337/api';
+export const PRESCHOOLAPP = '../exefiles/Pre-School-SoftTeacher.exe';
+export const GRADE1APP = '../exefiles/GradeOne-SoftTeacher.exe';
+export const GRADE2APP = '../exefiles/GradeTwo-SoftTeacher.exe';
